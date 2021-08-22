ccc2f75ee5472830f1758c0318b6c8d02c016ed0
Zi Ye <zi.ye1@thoughtworks.com>
2021-08-22 18:00:31 +0800

    click Square and then show 'X' in Square

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      value: 'X'
+    })
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={this.handleClick}>
+        {this.state.value}
       </button>
     );
   }
